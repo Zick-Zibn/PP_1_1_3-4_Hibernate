6e1b33f65eb218a76d6dd9f307d2bc5586d84e27
Zick_Zibn <karpenko_ilia@list.ru>
2026-01-22 13:39:19 +0300

    clear code

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,60 +2,21 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class Util {
-
-    // реализуйте настройку соеденения с БД
-    private static Connection connection;
-
-        static final String DB_URL = "jdbc:mysql://localhost:3306/test_db";
-        static final String USER = "jpauser";
-        static final String PASSWORD = "jpapwd";
-
-    public static Connection getConnection() {
-
-        try {
-
-            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return connection;
-    }
-
-    public static void closeConnection() {
-
-        try {
-
-            if (connection != null && !connection.isClosed()) {
-                connection.close();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
 
     public static SessionFactory getSessionFactory() {
         SessionFactory sessionFactory = null;
         try {
             Configuration configuration = new Configuration();
-            //configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+
             configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/test_db");
             configuration.setProperty("hibernate.connection.username", "jpauser");
             configuration.setProperty("hibernate.connection.password", "jpapwd");
-// добавьте классы или маппинг файлы
+
             configuration.addAnnotatedClass(User.class);
             sessionFactory = configuration.buildSessionFactory();
         } catch (Exception e) {
